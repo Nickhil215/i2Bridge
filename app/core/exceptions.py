4f5b653411ae5c3896c8d369f1d664475bd2682c
Nikhil Vanamala <105160327+Nickhil215@users.noreply.github.com>
2025-03-24 16:09:22 +0530

    Update exceptions.py

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -1,24 +1,24 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
-import logging
+# from fastapi import FastAPI, Request
+# from fastapi.responses import JSONResponse
+# import logging
 
-# Define the custom exception
-class ApiException(Exception):
-    def __init__(self, message: str, status_code: int = 500, detail: str = None):
-        self.message = message
-        self.detail = detail
-        self.status_code = status_code
+# # Define the custom exception
+# class ApiException(Exception):
+#     def __init__(self, message: str, status_code: int = 500, detail: str = None):
+#         self.message = message
+#         self.detail = detail
+#         self.status_code = status_code
 
-# Create the FastAPI application
-app = FastAPI()
+# # Create the FastAPI application
+# app = FastAPI()
 
-# Global exception handler for ApiException
-@app.exception_handler(ApiException)
-async def api_exception_handler(request: Request, exc: ApiException):
-    return JSONResponse(
-        status_code=exc.status_code,
-        content={
-            "message" : exc.message,
-            "detail": exc.detail
-        },
-    )
+# # Global exception handler for ApiException
+# @app.exception_handler(ApiException)
+# async def api_exception_handler(request: Request, exc: ApiException):
+#     return JSONResponse(
+#         status_code=exc.status_code,
+#         content={
+#             "message" : exc.message,
+#             "detail": exc.detail
+#         },
+#     )
